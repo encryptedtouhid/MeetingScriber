f11aec5bd617237c5d27af506ecb31bd5db74b1d
Khaled Md Tuhidul Hossain <me@tuhidulhossain.com>
2025-04-28 02:09:30 +0800

    removed

--- src/meetingscriber/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    SAMPLE_RATE = 44100
-    CHANNELS = 1
-    MODEL_TYPE = "base"  # Whisper model size
-    TRANSCRIPTION_LANGUAGE = "en"
-    USE_SPEAKER_DIARIZATION = False
--- src/meetingscriber/main.py
+++
@@ -1,4 +0,0 @@
-from src.meetingscriber.ui.cli import run_cli
-
-if __name__ == "__main__":
-    run_cli()
